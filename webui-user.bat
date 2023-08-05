82476251f6a6c46ca07400e1c965ab80cda2cd45
Yann <yannluca@naeher.eu>
2023-08-05 17:03:19 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --listen --medvram --xformers --no-progressbar-hiding --enable-console-prompts --administrator --enable-insecure-extension-access --no-half-vae --gradio-auth nya:meow,meow:nya
+set COMMANDLINE_ARGS= --listen --medvram --xformers --no-progressbar-hiding --enable-console-prompts --administrator --enable-insecure-extension-access --gradio-auth nya:meow,meow:nya
 
 call webui.bat
